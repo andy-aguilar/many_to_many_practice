9b8d153805a840f1246cba0f5f5b6460a3ccdaa8
andy-aguilar <aaguil3@gmail.com>
2020-06-09 12:59:26 -0400

    Started building tourist.rb

--- app/models/tourist.rb
+++ app/models/tourist.rb
@@ -1,1 +1,23 @@
 # put your Tourist model here
+class Tourist
+    attr_reader :name
+
+    @@all = []
+
+    def initialize (name)
+        @name = name
+        self.class.all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def self.find_by_name(name)
+        self.all.find do |tourist|
+            tourist.name == name
+        end
+    end
+    
+end
+
--- tools/console.rb
+++ tools/console.rb
@@ -2,4 +2,15 @@ require_relative '../config/environment.rb'
 
 # create your variables and/or write any tests here
 
-binding.pry
+luis = Tourist.new("Luis")
+andy = Tourist.new("Andy")
+
+puts "\n\n\n------------Tourist.all array---------------\n\n\n"
+
+print Tourist.all
+
+puts "\n\n\n------------Tourist.find_by_name---------------\n\n\n"
+
+print Tourist.find_by_name ("Luis")
+
+#binding.pry
